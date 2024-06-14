79321c1da4ddcb09acb5b1a79ca3d60fe4895498
Gabriela Mafuzo <112979830+gabimafuzo@users.noreply.github.com>
2024-06-14 10:34:30 -0300

    Update mario.c

--- Module1/mario.c
+++ Module1/mario.c
@@ -1,23 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main (){
+int main()
+{
     int height;
+    //Ask the height to the user until the condition is satisfied
     do
     {
-         height = get_int("height: ");
+        height = get_int("height: ");
+    }
+    while (height < 1 || height > 8);
+
+    //Build the pyramid
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height - 1 - i; j++)
+        {
+            printf(" ");
         }
-        while(height < 1 || height > 8);
 
-        for(int i = 0; i < height; i++){
-
-            for(int j = 0; j < height - 1 - i; j++){
-                printf(" ");
-            }
-
-            for(int k = 0; k < i + 1; k++){
+        for (int k = 0; k < i + 1; k++)
+        {
             printf("#");
         }
         printf("\n");
-        }
+    }
 }
