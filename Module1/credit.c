82640140baad707813c1d527d598a005ff37fbac
Gabriela Mafuzo <112979830+gabimafuzo@users.noreply.github.com>
2024-06-16 16:56:00 -0300

    Create credit.c
    In 'credit.c', i've implemented functionality that verifies the validity of a credit card number input by the user. The program begins by prompting the user for their credit card number, ensuring that the input is handled as a long integer using the 'get_long' function from the CS50 library.
    
    After receiving the input, the program applies Luhn's algorithm to determine if the credit card number is valid. Depending on the number's format and prefix, the program identifies whether it belongs to American Express, MasterCard, Visa, or is deemed invalid.
    
    To ensure compatibility with automated testing, the program strictly outputs one of the following lines based on its findings:
    
    AMEX\n for valid American Express cards,
    MASTERCARD\n for valid MasterCard cards,
    VISA\n for valid Visa cards,
    INVALID\n for numbers that do not match any valid card type.
    Luhn's algorithm validates identification numbers like credit cards by:
    
    1. Multiplying every second digit from the second-to-last by 2.
    2. Summing the digits of the results.
    3. Adding these sums to the digits not multiplied.
    4. Checking if the total sum % 10 equals 0 for validation."

---
+++ Module1/credit.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+string detectCardType(long n);
+
+int main()
+{
+    long number = get_long("Number: ");
+    long card_num = number;
+
+    // Luhn's Algorithm
+    int ysum = 0, xsum = 0, y, x, digits, i = 0;
+    while (number)
+    {
+        // When i % 2 equals 0 (false), the digit at index i is even; otherwise (1/true), it is odd
+        if (i % 2)
+        {
+            number = number / 10;
+        }
+        else //Access the digits at even positions.
+        {
+            // Step: Multiply every second digit by 2, starting with the second-to-last digit of the number (x), then sum the digits of these products
+            x = number % 100 / 10;
+            x = 2 * x;
+            digits = x % 10 + x % 100 / 10;
+            xsum += digits;
+
+            //Sum of the digits that were not multiplied by 2 (y).
+            y = number % 10; //soma dos outros
+            ysum += y;
+
+
+            number = number / 10;
+        }
+        i++;
+
+    }
+
+    if ((ysum + xsum) % 10) // If the result of (ysum + xsum) modulo 10 is not equal to 0, this condition is true
+    {
+        printf("INVALID\n");
+    }
+    else
+    {
+        string cardType = detectCardType(card_num);
+        printf("%s\n", cardType);
+    }
+}
+
+string detectCardType(long n)
+{
+    int j = 2;
+    string cardType;
+    while (n >= 100) //To access only the first two digits of the card
+    {
+        n = n / 10;
+        j++; //Digit counter (correction factor of 2)
+    }
+    if (j == 15 && (n == 34 || n == 37))
+    {
+        cardType = "AMEX";
+        return cardType;
+    }
+    else if (j == 16 && (n == 51 || n == 52 || n == 53 || n == 54 || n == 55))
+    {
+        cardType = "MASTERCARD";
+        return cardType;
+    }
+    else if (j >= 13 && j <= 16 && n / 10 == 4)
+    {
+        cardType = "VISA";
+        return cardType;
+    }
+    else
+    {
+        cardType = "INVALID";
+        return cardType;
+    }
+}
